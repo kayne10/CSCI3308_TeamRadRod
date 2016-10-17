fba4c449eaa5d12b17596a8198e6d571ea565af1
kayne10 <tkayne10@gmail.com>
2016-10-17 16:23:08 -0600

    Initialized Database

--- ProductivityManagementApp/ProductivityManagementApp/settings.py
+++ ProductivityManagementApp/ProductivityManagementApp/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'ProductivityManagementApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TeamRadRod',
+        'USER': 'root',
+        'PASSWORD': 'buzchop1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
--- ProductivityManagementApp/courses/models.py
+++ ProductivityManagementApp/courses/models.py
@@ -1,5 +1,12 @@
-from __future__ import unicode_literals
-
+from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=400)
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+		return self.title
