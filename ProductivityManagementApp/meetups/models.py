8d85334d32750073d1263bc79a7d118d519afde7
kayne10 <tkayne10@gmail.com>
2016-10-25 11:42:14 -0600

    Fixed Meetup Table in Database

--- ProductivityManagementApp/meetups/models.py
+++ ProductivityManagementApp/meetups/models.py
@@ -11,11 +11,11 @@ class MeetUp(models.Model):
     title = models.CharField(max_length=30)
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    location = models.CharField(max_length=30)
     # We want to invite users to this model. Or we can show that a user created a meetup and make it public??
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     start_time = models.DateTimeField(auto_now=False)
-    end_time = models.DateTimeField(auto_now=False)
+    est_end_time = models.DateTimeField(auto_now=False)
+    location = models.CharField(max_length=20)
 
     def __str__(self):
         return self.title + ' - ' + self.assignment + ' - ' + self.course
